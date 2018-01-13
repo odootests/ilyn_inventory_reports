ad9c6d2a6c115e3c8cfc84a1eed0af3eb5b3be01
Toufiq <burningraven06@gmail.com>
2018-01-13 10:38:16 +0600

    pushing code

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "ILYN Inventory Reports",
+    'version': "10.0.1.0.0",
+    'author': "Haha",
+    'depends': ['stock'],
+    'installable': True,
+    'data': ['current_inventory.xml'],
+    'description': "Very Long Description"
+}
